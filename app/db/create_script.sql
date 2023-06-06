b9fd3b3cc9e0a3d3be019abb8f03c358d96b7fc9
Amine Azzamouri <amine.azzamouri@gamil.com>
2023-06-06 15:24:43 +0200

    final

--- app/db/create_script.sql
+++ app/db/create_script.sql
@@ -1,45 +1,45 @@
 -- Step: 01
--- Goal: Create a new database mvc-2209a-P4
+-- Goal: Create a new database mvc-2209a-P4-Toets
 -- **********************************************************************************
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
 -- Check if the database exists
-DROP DATABASE IF EXISTS `Mvc-2209a-P4`;
+DROP DATABASE IF EXISTS `Mvc-2209a-P4-Toets`;
 
 -- Create a new Database
-CREATE DATABASE IF NOT EXISTS `Mvc-2209a-P4`;
+CREATE DATABASE IF NOT EXISTS `Mvc-2209a-P4-Toets`;
 
 -- Use database Mvc-2209a-P4
-Use `Mvc-2209a-P4`;
+Use `Mvc-2209a-P4-Toets`;
 
 
 -- Step: 02
--- Goal: Create a new table Instructeur
+-- Goal: Create a new table Examen
 -- **********************************************************************************
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
 -- Drop table Instructeur
-DROP TABLE IF EXISTS Instructeur;
+DROP TABLE IF EXISTS Examen;
 
-CREATE TABLE IF NOT EXISTS Instructeur
+CREATE TABLE IF NOT EXISTS Examen
 (
-    Id              TINYINT         UNSIGNED        NOT NULL    AUTO_INCREMENT
-   ,Voornaam        VARCHAR(50)                     NOT NULL
-   ,Tussenvoegsel   VARCHAR(10)                     NOT NULL
-   ,Achternaam      VARCHAR(50)                     NOT NULL
-   ,Mobiel          VARCHAR(12)                     NOT NULL
-   ,DatumInDienst   DATE                            NOT NULL
-   ,AantalSterren   VARCHAR(6)                      NOT NULL
-   ,IsActief        BIT                             NOT NULL    DEFAULT 1
-   ,Opmerkingen     VARCHAR(250)                        NULL    DEFAULT NULL
-   ,DatumAangemaakt DateTime(6)                     NOT NULL
-   ,DatumGewijzigd  DateTime(6)                     NOT NULL
+    Id              			TINYINT         UNSIGNED        NOT NULL    AUTO_INCREMENT
+   ,StudentId       			VARCHAR(50)                     NOT NULL
+   ,Rijschool   				VARCHAR(10)                     NOT NULL
+   ,Stad      					VARCHAR(50)                     NOT NULL
+   ,Rijbewijscategorie          VARCHAR(12)                     NOT NULL
+   ,Datum   					DATE                            NOT NULL
+   ,Uitslag   					VARCHAR(6)                      NOT NULL
+   ,IsActief        			BIT                             NOT NULL    DEFAULT 1
+   ,Opmerkingen     			VARCHAR(250)                        NULL    DEFAULT NULL
+   ,DatumAangemaakt 			DateTime(6)                     NOT NULL
+   ,DatumGewijzigd  			DateTime(6)                     NOT NULL
 
    ,CONSTRAINT      PK_Instructeur_Id   PRIMARY KEY CLUSTERED(Id)
 ) ENGINE=InnoDB;
@@ -51,47 +51,49 @@ CREATE TABLE IF NOT EXISTS Instructeur
 
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
-INSERT INTO Instructeur
+INSERT INTO Examen
 (
-     Voornaam
-    ,Tussenvoegsel
-    ,Achternaam
-    ,Mobiel
-    ,DatumInDienst
-    ,AantalSterren
-    ,IsActief
-    ,Opmerkingen
-    ,DatumAangemaakt
-    ,DatumGewijzigd
+	StudentId       			
+   ,Rijschool   				
+   ,Stad      					
+   ,Rijbewijscategorie          
+   ,Datum   					
+   ,Uitslag   					
+   ,IsActief        			
+   ,Opmerkingen     			
+   ,DatumAangemaakt 			
+   ,DatumGewijzigd  
 )
 VALUES
-     ('Li', '', 'Zhan', '06-28493827', '2015-04-17', '***', 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('Leroy', '', 'Boerhaven', '06-39398734', '2018-06-25', '*', 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('Yoeri', 'van', 'Veen', '06-24383291', '2010-05-12', '***', 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('Bert', 'van', 'Sali', '06-48293823', '2023-01-10', '****', 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('Mohammed', 'El', 'Yassidi', '06-34291234', '2010-06-14', '*****', 1, NULL, SYSDATE(6), SYSDATE(6));
+     ('100234', 'VolGasVooruit', 'Rotterdam', 'B', '2023-03-04', 'Geslaagd' 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('123432', 'InZijnVierDoorDeBocht', 'Sliedrecht', 'C', '2023-01-03', 'Geslaagd' 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('103234', 'LinksomRechtsom', 'Dordrecht', 'D', '2023-15-05', 'Geslaagd' 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('106452', 'OpDeVluchtStrook', 'Zwijndrecht', 'AM', '2023-08-05', 'Gezakt', 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('104546', 'RechtDoorEnGaan', 'Rotterdam', 'B', '2023-17-04', 'Gezakt' 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('100333', 'AltijdGeslaagd', 'Dordrecht', 'B', '2023-12-05', 'Geslaagd', 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('124444', 'RijlesVoorJou', 'Rotterdam', 'B', '2023-12-04', 'Geslaagd' 1, NULL, SYSDATE(6), SYSDATE(6));
 
 
 
 -- Step: 04
--- Goal: Create a new table TypeVoertuig
+-- Goal: Create a new table ExamenPerExaminator
 -- **********************************************************************************
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
 -- Drop table TypeVoertuig
-DROP TABLE IF EXISTS TypeVoertuig;
+DROP TABLE IF EXISTS ExamenPerExaminator;
 
-CREATE TABLE IF NOT EXISTS TypeVoertuig
+CREATE TABLE IF NOT EXISTS ExamenPerExaminator
 (
     Id                  TINYINT         UNSIGNED        NOT NULL    AUTO_INCREMENT
-   ,TypeVoertuig        VARCHAR(20)                     NOT NULL
-   ,Rijbewijscategorie  VARCHAR(5)                      NOT NULL
+   ,ExamenId        	VARCHAR(20)                     NOT NULL
+   ,ExaminatorId  		VARCHAR(5)                      NOT NULL
    ,IsActief            BIT                             NOT NULL    DEFAULT 1
    ,Opmerkingen         VARCHAR(250)                        NULL    DEFAULT NULL
    ,DatumAangemaakt     DateTime(6)                     NOT NULL
@@ -107,23 +109,26 @@ CREATE TABLE IF NOT EXISTS TypeVoertuig
 
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
-INSERT INTO TypeVoertuig
+INSERT INTO ExamenPerExaminator
 (
-     TypeVoertuig
-    ,Rijbewijscategorie
-    ,IsActief
-    ,Opmerkingen
-    ,DatumAangemaakt
-    ,DatumGewijzigd
+	ExamenId        	
+   ,ExaminatorId  		
+   ,IsActief           
+   ,Opmerkingen         
+   ,DatumAangemaakt     
+   ,DatumGewijzigd      
 )
 VALUES
-     ('Personenauto', 'B', 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('Vrachtwagen', 'C', 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('Bus', 'D', 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('Bromfiets', 'AM', 1, NULL, SYSDATE(6), SYSDATE(6));
+     ('1', '3', 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('3', '3', 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('2', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('4', '1', 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('7', '3', 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('6', '2', 1, NULL, SYSDATE(6), SYSDATE(6))
+    ,('5', '4', 1, NULL, SYSDATE(6), SYSDATE(6));
 
 
 
@@ -132,20 +137,19 @@ VALUES
 -- **********************************************************************************
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
 -- Drop table Voertuig
-DROP TABLE IF EXISTS Voertuig;
+DROP TABLE IF EXISTS Examinator;
 
-CREATE TABLE IF NOT EXISTS Voertuig
+CREATE TABLE IF NOT EXISTS Examinator
 (
     Id                  TINYINT         UNSIGNED        NOT NULL    AUTO_INCREMENT
-   ,Kenteken            VARCHAR(12)                     NOT NULL
-   ,Type                VARCHAR(20)                     NOT NULL
-   ,Bouwjaar            DATE                            NOT NULL
-   ,Brandstof           VARCHAR(20)                     NOT NULL  
-   ,TypeVoertuigId      TINYINT         UNSIGNED        NOT NULL  
+   ,Voornaam            VARCHAR(12)                     NOT NULL
+   ,Tussenvoegsel       VARCHAR(20)                     NOT NULL
+   ,Achternaam          DATE                            NOT NULL
+   ,Mobiel          	VARCHAR(20)                     NOT NULL  
    ,IsActief            BIT                             NOT NULL    DEFAULT 1
    ,Opmerkingen         VARCHAR(250)                        NULL    DEFAULT NULL
    ,DatumAangemaakt     DateTime(6)                     NOT NULL
@@ -162,33 +166,24 @@ CREATE TABLE IF NOT EXISTS Voertuig
 
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
-INSERT INTO Voertuig
+INSERT INTO Examinator
 (
-     Kenteken
-    ,Type
-    ,Bouwjaar
-    ,Brandstof
-    ,TypeVoertuigId
-    ,IsActief
-    ,Opmerkingen
-    ,DatumAangemaakt
-    ,DatumGewijzigd
+   Voornaam           
+   ,Tussenvoegsel     
+   ,Achternaam        
+   ,Mobiel          
+   ,IsActief           
+   ,Opmerkingen        
+   ,DatumAangemaakt     
+   ,DatumGewijzigd    
 )
 VALUES
      ('AU-67-IO', 'Golf', '2017-06-12', 'Diesel', 1, 1, NULL, SYSDATE(6), SYSDATE(6))
     ,('TR-24-OP', 'DAF', '2019-05-23', 'Diesel', 2, 1, NULL, SYSDATE(6), SYSDATE(6))
     ,('TH-78-KL', 'Mercedes', '2023-01-01', 'Benzine', 1, 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('90-KL-TR', 'Fiat 500', '2021-09-12', 'Benzine', 1, 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('34-TK-LP', 'Scania', '2015-03-13', 'Diesel', 2, 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('YY-OP-78', 'BMW M5', '2022-05-13', 'Diesel', 1, 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('UU-HH-JK', 'M.A.N', '2017-12-03', 'Diesel', 2, 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('ST-FZ-28', 'Citroën', '2018-01-20', 'Elektrisch', 1, 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('123-FR-T', 'Piaggio ZIP', '2021-02-01', 'Benzine', 4, 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('DRS-52-P', 'Vespa', '2022-03-21', 'Benzine', 4, 1, NULL, SYSDATE(6), SYSDATE(6))
-    ,('STP-12-U', 'Vespa', '2022-07-02', 'Benzine', 4, 1, NULL, SYSDATE(6), SYSDATE(6))
     ,('45-SD-23', 'Renault', '2023-01-01', 'Diesel', 3, 1, NULL, SYSDATE(6), SYSDATE(6));
 
 
@@ -198,7 +193,7 @@ VALUES
 -- **********************************************************************************
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
 -- Drop table VoertuigInstructeur
@@ -227,7 +222,7 @@ CREATE TABLE IF NOT EXISTS VoertuigInstructeur
 
 -- Version       Date:           Author:                     Description:
 -- *******       **********      ****************            ******************
--- 01            20-05-2023      Amine Azzamouri            New
+-- 01            30-05-2023      Amine Azzamouri            New
 -- **********************************************************************************/
 
 INSERT INTO VoertuigInstructeur
